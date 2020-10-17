b6b3b34ef7665993708e4fd944425b4be49128ba
Ruslan Fatykhov <rfatikhov@outlook.com>
2020-10-17 14:40:56 +0300

    add layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,20 @@ import { AppContainer, CardContainer } from "./styles";
 import { Column } from "./components/Column";
 import { Card } from "./components/Card";
 
-function App() {
+const App = () => {
   return (
     <AppContainer>
-      <Column />
+      <Column text="To Do">
+        <Card text="Build Kanban Board" />
+      </Column>
+      <Column text="In Progress">
+        <Card text="Learn TypeScript" />
+      </Column>
+      <Column text="Done">
+        <Card text="Learn React" />
+      </Column>
     </AppContainer>
   );
-}
+};
 
 export default App;
--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { ColumnContainer, ColumnTitle } from "../styles";
 
-interface ColumnProp {
+interface ColumnProps {
   text: string;
 }
 
-export const Column = ({ text }: ColumnProp) => {
+export const Column = ({
+  text,
+  children,
+}: React.PropsWithChildren<ColumnProps>) => {
   return (
     <ColumnContainer>
       <ColumnTitle>{text}</ColumnTitle>
+      {children}
     </ColumnContainer>
   );
 };
